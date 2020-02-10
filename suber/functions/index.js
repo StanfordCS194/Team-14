40d3cc39874ee9834cfb0366db0a7bf6803e8b68
ynkwon <ynkwon@stanford.edu>
2020-02-09 22:13:56 -0800

    hi

--- suber/functions/index.js
+++ suber/functions/index.js
@@ -5,19 +5,29 @@ const app = express();
 const FBAuth = require('./util/fbAuth');
 
 const {getAllScreams, postOneScream} = require('./handlers/screams');
-const {signup, login, uploadImage} = require('./handlers/users');
+const {signup, login, uploadImage, addUserDetails}, getAuthenticatedUser = require('./handlers/users');
 
 
 
 //Screams route
 app.get('/screams', getAllScreams)
 app.post('/screams', FBAuth, postOneScream);
+app.get('/scream/:screamId', getScream);
+//TODO : delete scream
+//TODO : like scream
+//TODO : unlike scream
+//TODO : comment on scream
+
 
 //signup route
 app.post('/signup', signup);
 //login route
 app.post('/login', login)
-app.post('/user/image', uploadImage)
+
+app.post('/user/image', FBAuth, uploadImage)
+app.post('/user', FBAuth, addUserDetails);
+app.get('/user', FBAuth, getAuthenticatedUser);
+
 
 
 //functions
