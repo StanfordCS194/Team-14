13222eb889dafd6d2a0756f427a3b9c1cd9b14c8
Jonathan Kim <jonkim95@stanford.edu>
2020-02-17 14:37:43 -0800

    Add search-tour-by-guide API

--- suber/functions/handlers/tour.js
+++ suber/functions/handlers/tour.js
@@ -47,8 +47,8 @@ exports.postNewTour = (req, res) => {
   
     admin
     .firestore()
-    .collection('upcoming_tours')
-    .add(newScream)
+    .collection('ours')
+    .add(newTour)
     .then((doc) => {
       res.json({message: `document ${doc.id} created successfully`});
     })
@@ -56,12 +56,40 @@ exports.postNewTour = (req, res) => {
       res.status(500).json({error: 'something went wrong'});
       console.error(err);
     });
-  
   }
 
   exports.getTourForTourist = (req, res) => {
-    admin.firestore().collection('upcoming_tours')
+    admin.firestore().collection('tours')
         .orderBy('touristContact').equalTo(req.touristContact)
+        .get()
+        .then((data) => {
+            let tours = [];
+            data.forEach((doc) => {
+                tours.push({
+                tourId: doc.id,
+                status: doc.data().status,
+                uid: doc.data().uid,
+                touristName: doc.data().touristName,
+                touristContact: doc.data().touristContact,
+                startTime: doc.data().startTime,
+                endTime: doc.data().endTime,
+                startLoc: doc.data().startLoc,
+                language: doc.data().language,
+                partySize: doc.data().partySize,
+                note: doc.data().note,
+            });
+        });
+        return res.json(tours);
+     })
+     .catch((err) => {
+         console.error(err);
+         res.status(500).json({error: err.code});
+     });
+
+  exports.getTourForGuide = (req, res) => {
+    admin.firestore().collection('tours')
+        .orderBy('uid').equalTo(req.uid)
+        .orderBy('status').equalTo(req.status)
         .get()
         .then((data) => {
             let tours = [];
