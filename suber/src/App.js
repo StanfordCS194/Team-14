2eb266d8bee97519f7fd9f001f0e6f13dff1264a
ynkwon <ynkwon@stanford.edu>
2020-02-17 23:57:04 -0800

    hi

--- suber/src/App.js
+++ suber/src/App.js
@@ -1,15 +1,15 @@
-import React from "react";
-import { HashRouter, Route } from "react-router-dom";
-import Home from "./Home";
-import Tour from "./Tour";
-import Tourguide from "./Tourguide"
+import React from 'react';
+import { HashRouter, Route } from 'react-router-dom';
+import Home from './components/Home/Home';
+import Tour from './components/Tour/Tour';
+import Tourguide from './components/Tourguide/Tourguide'
 
 function App() {
   return (
     <HashRouter>
-      <Route path="/" exact={true} component={Home} />
-      <Route path="/tour" component={Tour} />
-      <Route path="/tourguide" component={Tourguide} />
+      <Route path='/' exact={true} component={Home} />
+      <Route path='/tour' component={Tour} />
+      <Route path='/tourguide' component={Tourguide} />
     </HashRouter>
   );
 }
