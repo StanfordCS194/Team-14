0ab8f975b2e1206f7410d66b61d8acba7fec066d
Jonathan Kim <jonkim95@stanford.edu>
2020-03-08 16:36:33 -0700

    Update guides API to retrieve startLoc and schedule

--- suber/functions/handlers/guides.js
+++ suber/functions/handlers/guides.js
@@ -2,29 +2,32 @@ const {admin, db} = require('../util/admin');
 
 exports.getAllGuides = (req, res) => {
     admin.firestore().collection('users')
-        .orderBy('firstName')
-        .get()
-        .then((data) => {
-            let guides = [];
-            data.forEach((doc) => {
-                guides.push({
-                    userId: doc.data().userId,
-                    firstName: doc.data().firstName,
-                    lastName: doc.data().lastName,
-                    imageUrl: doc.data().imageUrl,
-                    handle: doc.data().handle,
-                    email: doc.data().email,
-                    language: doc.data().language,
-                    major: doc.data().major,
-                    completedTours: doc.data().completedTours,
-                    netRating: doc.data().netRating,
-                    note: doc.data().note
+    .orderBy('firstName')
+    .get()
+    .then((data) => {
+        let guides = [];
+        data.forEach((doc) => {
+            guides.push({
+                userId: doc.data().userId,
+                firstName: doc.data().firstName,
+                lastName: doc.data().lastName,
+                imageUrl: doc.data().imageUrl,
+                handle: doc.data().handle,
+                email: doc.data().email,
+                language: doc.data().language,
+                major: doc.data().major,
+                startLoc: doc.data().startLoc,
+                schedule: doc.data().schedule,
+                phone: doc.data().phone,
+                completedTours: doc.data().completedTours,
+                netRating: doc.data().netRating,
+                note: doc.data().note
             });
         });
         return res.json(guides);
-     })
-     .catch((err) => {
-         console.error(err);
-         res.status(500).json({error: err.code});
-     });
+    })
+    .catch((err) => {
+        console.error(err);
+        res.status(500).json({error: err.code});
+    });
 }
