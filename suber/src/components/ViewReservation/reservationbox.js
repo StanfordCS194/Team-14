49e355f5e1346bea9ca435e17197d458a878120b
Jin Woo Yu <scientistsimon@hotmail.com>
2020-03-08 22:08:35 -0700

    Update reservationbox.js

--- suber/src/components/ViewReservation/reservationbox.js
+++ suber/src/components/ViewReservation/reservationbox.js
@@ -7,8 +7,59 @@ class ReservationBox extends React.Component {
     render() {
         return (
 		<div id="res_box">
-			
-		</div>
+			<h3 id="res_time">
+				February 28th, 2020 1830 1930
+			</h3>
+			<h3 id="tourists">
+				Collin Kwon and 2 others
+			</h3>
+			<hr id="res_divider"></hr>
+			<div id = "res_details">
+				<div class = "res_detail_title">
+					Email
+				</div>
+				<div class = "res_detail_contents">
+					ynkwon@stanford.edu
+				</div>
+				<div class = "res_detail_title">
+				Mobile
+				</div>
+				<div class = "res_detail_contents">
+				650-709-7655
+				</div>
+				<div class = "res_detail_title">
+				# of People
+				</div>
+				<div class = "res_detail_contents">
+				4
+				</div>
+				<div class = "res_detail_title">
+				Language
+				</div>
+				<div class = "res_detail_contents">
+				Mandarin
+				</div>
+				<div class = "res_detail_title">
+				Meetup Location
+				</div>
+				<div class = "res_detail_contents">
+				Main Quad
+				</div>
+				<div class = "res_detail_title">
+				Price
+				</div>
+				<div class = "res_detail_contents">
+				$60
+				</div>
+				<div class = "res_detail_title">
+				Additional Request
+				</div>
+				<div class = "res_detail_contents">
+				Bunch of text goes here. Bunch of text goes here. Bunch of text goes here. Bunch of text goes here. Bunch of text goes here. Bunch of text goes here. Bunch of text goes here. Bunch of text goes here. Bunch of text goes here. Bunch of text goes here. Bunch of text goes here. Bunch of text goes here. Bunch of text goes here. Bunch of text goes here. Bunch of text goes here. 
+				</div>	
+
+			</div>	
+		</div> 
     	)
 	}
 }
