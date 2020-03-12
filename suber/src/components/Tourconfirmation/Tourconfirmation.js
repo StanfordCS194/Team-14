3931f3731523c6b597800daf3c0b5013d6350358
jihun-hong <hjihun@stanford.edu>
2020-03-11 23:45:44 -0700

    fix options in home page for suber

--- suber/src/components/Tourconfirmation/Tourconfirmation.js
+++ suber/src/components/Tourconfirmation/Tourconfirmation.js
@@ -6,57 +6,6 @@ import StarRatings from "react-star-ratings";
 import suberimg from '../../imgs/SUBER.png';
 import mapimg from '../../imgs/sample-map.png';
 
-const options_time = [
-    { value: '0500', label: '05:00' },
-    { value: '0530', label: '05:30' },
-    { value: '0600', label: '06:00' },
-    { value: '0630', label: '06:30' },
-    { value: '0700', label: '07:00' },
-    { value: '0730', label: '07:30' },
-    { value: '0800', label: '08:00' },
-    { value: '0830', label: '08:30' },
-    { value: '0900', label: '09:00' }
-];
-
-const options_duration = [
-    { value: '05', label: '0.5 hour' },
-    { value: '10', label: '1 hour' },
-    { value: '15', label: '1.5 hour' },
-    { value: '20', label: '2 hour' },
-    { value: '25', label: '2.5 hour' },
-    { value: '30', label: '3 hour' },
-    { value: '35', label: '3.5 hour' },
-    { value: '40', label: '4 hour' },
-    { value: '45', label: '4.5 hour' },
-    { value: '50', label: '5 hour' }
-];
-
-const options_size = [
-    { value: '1', label: '1' },
-    { value: '2', label: '2' },
-    { value: '3', label: '3' },
-    { value: '4', label: '4' },
-    { value: '5', label: '5' },
-    { value: '6', label: '6' }
-];
-
-const options_language = [
-    { value: '1', label: 'English' },
-    { value: '2', label: 'Spanish' },
-    { value: '3', label: 'Chinese' },
-    { value: '4', label: 'Japanese' },
-    { value: '5', label: 'Korean' },
-    { value: '6', label: 'Hindi' }
-];
-
-const options_major = [
-    { value: '1', label: 'Computer Science' },
-    { value: '2', label: 'Engineering' },
-    { value: '3', label: 'Social Sciences' },
-    { value: '4', label: 'Humanities' },
-    { value: '5', label: 'Other' }
-];
-
 class Tourconfirmation extends React.Component {
     state = {
         startDate: new Date(),
